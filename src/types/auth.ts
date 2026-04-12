90fe9a4aa8508a351974d184b61b48391c84998e
Sangam <100322380+sangT220222@users.noreply.github.com>
2026-04-12 13:22:28 +0100

    feature(api) - ticket ownership, and route restriction (#9)

--- src/types/auth.ts
+++ src/types/auth.ts
@@ -1,5 +1,17 @@
+import { Request } from "express";
+import type { ParamsDictionary } from "express-serve-static-core";
+
 export type AuthenticatedUser = {
   id: string;
   email: string;
   role: string;
+};
+
+export type AuthenticatedRequest<
+  P = ParamsDictionary,
+  ResBody = any,
+  ReqBody = any,
+  ReqQuery = any
+> = Request<P, ResBody, ReqBody, ReqQuery> & {
+  user: AuthenticatedUser;
 };
