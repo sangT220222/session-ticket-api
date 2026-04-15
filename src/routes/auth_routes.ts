6e5eb6e9a2166d019317a25105b5d64bdc5a29fb
Sangam <100322380+sangT220222@users.noreply.github.com>
2026-04-15 10:36:20 +0100

    feature(api): helmet and express-rate-limit added to enhance security of the API

--- src/routes/auth_routes.ts
+++ src/routes/auth_routes.ts
@@ -1,4 +1,15 @@
 import { Router } from "express";
+import rateLimit from "express-rate-limit";
+
+const limiter = rateLimit({
+  windowMs: 60 * 1000, // 1 minutes
+  limit: 5, // Limit each IP to 5 requests per `window` (here, per 15 minutes).
+  //limit to 5 requests per 15 minutes
+  standardHeaders: "draft-8", // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+  ipv6Subnet: 56, // Set to 60 or 64 to be less aggressive, or 52 or 48 to be more aggressive
+  // store: ... , // Redis, Memcached, etc. See below.
+});
 import {
   validateLoginBody,
   validateRegisterBody,
@@ -21,6 +32,7 @@ authRouter.post(
 
 authRouter.post(
   "/login",
+  limiter,
   validateLoginBody(loginUserSchema),
   loginUserController
 );
