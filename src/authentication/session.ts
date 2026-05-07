9adb22dac788aaa96789890eb281a3999b86ea71
Sangam <100322380+sangT220222@users.noreply.github.com>
2026-05-07 10:12:54 +0100

    feature(api): Redis implementation (#15)

--- src/authentication/session.ts
+++ src/authentication/session.ts
@@ -1,12 +1,32 @@
 import session from "express-session";
 import dotenv from "dotenv";
+import { RedisStore } from "connect-redis";
+import { createClient } from "redis";
 
 dotenv.config();
+
+if (!process.env.REDIS_URL) {
+  throw new Error("REDIS_URL is missing");
+}
+
+//intialising client
+const redisClient = createClient({
+  url: process.env.REDIS_URL,
+});
+redisClient.connect().catch(console.error);
+
+//initialising store
+let redisStore = new RedisStore({
+  client: redisClient,
+  prefix: "myapp:",
+  // ttl: 60 //ttl is in seconds - this can be set explicitly, however, it usually derives from cookie.maxAge automatically
+});
 
 if (!process.env.SESSION_SECRET) {
   throw new Error("EMPTY_SECRET_KEY");
 }
 export const sessionMiddleware = session({
+  store: redisStore,
   secret: process.env.SESSION_SECRET,
   resave: false, //Minimal DB hits -> no data saving in DB if no changes been made in requests
   saveUninitialized: false, //no initial data saving if empty data at the start
@@ -14,7 +34,7 @@ export const sessionMiddleware = session({
     secure: process.env.NODE_ENV === "production",
     httpOnly: true,
     sameSite: "lax",
-    maxAge: 15 * 60 * 1000, //15 minutes
+    maxAge: 60 * 1000, //1 minute for test
   },
   //secure -> HTTPS only connection
   //putt secure:false for dev env
