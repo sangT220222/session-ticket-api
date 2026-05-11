706c7f41e3f0cb531ce03dab1057c75a8ed5bd6a
Sangam <100322380+sangT220222@users.noreply.github.com>
2026-05-11 13:40:46 +0100

    feat(session): configure session cookie name

--- src/authentication/session.ts
+++ src/authentication/session.ts
@@ -22,11 +22,19 @@ let redisStore = new RedisStore({
   // ttl: 60 //ttl is in seconds - this can be set explicitly, however, it usually derives from cookie.maxAge automatically
 });
 
+if (!process.env.SESSION_COOKIE_NAME) {
+  throw new Error("EMPTY_SESSION_NAME");
+}
+
 if (!process.env.SESSION_SECRET) {
   throw new Error("EMPTY_SECRET_KEY");
 }
+
+export const SESSION_COOKIE_NAME = process.env.SESSION_COOKIE_NAME;
+
 export const sessionMiddleware = session({
   store: redisStore,
+  name: process.env.SESSION_COOKIE_NAME,
   secret: process.env.SESSION_SECRET,
   resave: false, //Minimal DB hits -> no data saving in DB if no changes been made in requests
   saveUninitialized: false, //no initial data saving if empty data at the start
